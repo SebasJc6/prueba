bd06fd34599ec96c4fa8273a66b9c0b48599bddb
M-Narvaez-SOAINT <113195440+M-Narvaez-SOAINT@users.noreply.github.com>
2022-10-13 01:19:49 -0500

    Ajustes Solicitud de Modificacion

--- src/app/Views/PAA/requirements/modification-request/modification-request.component.ts
+++ src/app/Views/PAA/requirements/modification-request/modification-request.component.ts
@@ -370,8 +370,6 @@ export class ModificationRequestComponent implements OnInit {
           });
   }
 
-
-
   newRequeriment() {
     this.router.navigate([`PAA/PropiedadesRequerimiento/${this.dataProjectID}/${this.dataSolicitudModID}/${this.ID_REQUERIMIENTO}/Nuevo`]);
   }
@@ -940,11 +938,11 @@ export class ModificationRequestComponent implements OnInit {
 
   //Botón Enviar
   enviar() { 
-    let arrayDataSave: postDataModReqI[] = [];
     let fromStorageArrayData = ProChartStorage.getItem(`arrayDatos${this.dataSolicitudModID}`);
-    if (fromStorageArrayData != null) {
+    let fromStorageCounters = ProChartStorage.getItem(`arrayCounterparts${this.dataSolicitudModID}`);
+    if (fromStorageArrayData != null && fromStorageCounters != null) {
       this.openSnackBar('Lo sentimos', `No se puede enviar la solicitud`, 'error', `Debe guardar primero todos los registros.`);
-    }else {
+    }else if (this.StatusRequest == 'Modificacion' || this.StatusRequest == 'Ajuste'){
 
       let sendData = {
         idProyecto: this.dataProjectID,
@@ -980,6 +978,8 @@ export class ModificationRequestComponent implements OnInit {
           console.log('Hubo un error ', error);
           
         });
+    } else {
+      this.openSnackBar('Lo sentimos', `No se puede enviar la solicitud`, 'error', `Debe estar en estado "En Modificación" ó "En Ajuste" para ser enviada.`);
     }
   }
 
