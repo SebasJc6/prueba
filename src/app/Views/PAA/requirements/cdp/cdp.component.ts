8944893854dcf66fdf9da37cbe6386722da05f66
M-Narvaez-SOAINT <113195440+M-Narvaez-SOAINT@users.noreply.github.com>
2022-10-18 17:20:48 -0500

    Creacion de vista CDP

--- src/app/Views/PAA/requirements/cdp/cdp.component.ts
+++ src/app/Views/PAA/requirements/cdp/cdp.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { ActivatedRoute } from '@angular/router';
+import { ModificationRequestService } from 'src/app/Services/ServicesPAA/modificationRequest/modification-request.service';
 
 @Component({
   selector: 'app-cdp',
@@ -7,9 +11,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CDPComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activeRoute: ActivatedRoute,
+    private serviceModRequest: ModificationRequestService,) { }
+
+  displayedColumns: string[] = [
+    'Vigencia',
+    'CDP',
+    'FECHA CDP',
+    'VALOR INICIAL',
+    'VALOR ANULACIÓN',
+    'FECHA ANULACIÓN',
+    'VALOR FINAL',
+    'No. RP',
+    'VALOR RP',
+    'VALOR DISTRIBUIDO'
+  ];
+
+  dataSource: any[] = []; //TODO: Crear interfaces similares a Modification Sumary
+
+  dataProjectID: string = '';
+  requerimentId: string = '';
+
+  //Valores para guardar la informacion del proyecto para mostrar en la miga de pan
+  codProject: number = 0;
+  nomProject: string = ''; 
+
+  //Paginación
+  pageSummary = {} as any; //TODO: Crear interfaces similares a Modification Sumary
+
+  paginationForm = new FormGroup({
+    page: new FormControl(),
+    take: new FormControl()
+  });
+
+  pageSizeOptions: number[] = [5, 10, 15, 20];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  numberPages: number = 0;
+  numberPage: number = 0;
 
   ngOnInit(): void {
+    this.dataProjectID = this.activeRoute.snapshot.paramMap.get('idPro') || '';
+    this.requerimentId = this.activeRoute.snapshot.paramMap.get('idReq') || '';
+    this.getModificationRequet(Number(this.dataProjectID));
   }
 
+  //Obtener la información del proyecto para mostrar en miga de pan
+  getModificationRequet(projectId: number) {
+    this.serviceModRequest.getModificationRequest(projectId).subscribe((data) => {
+      this.codProject = data.data.proyecto_COD;
+      this.nomProject = data.data.nombreProyecto;
+    });
+  }
+
+
+  //PAGINACIÓN
+  getPagination() {
+    this.pageSummary.page = this.paginationForm.get('page')?.value;
+    this.pageSummary.take = this.paginationForm.get('take')?.value;
+    // this.getSummary(Number(this.dataSolicitudModID),this.selectedValueSource, this.pageSummary);
+  }
+
+  nextPage() {
+    if (this.numberPage < this.numberPages) {
+      this.numberPage++;
+      this.pageSummary.page = this.numberPage.toString();
+      // this.getSummary(31,this.selectedValueSource, this.pageSummary);
+    }
+  }
+
+
+  prevPage() {
+    if (this.numberPage > 1) {
+      this.numberPage--;
+      this.pageSummary.page = this.numberPage.toString();
+      // this.getSummary(31,this.selectedValueSource, this.pageSummary);
+    }
+  }
+
+  firstPage() {
+    this.numberPage = 1;
+    this.pageSummary.page = this.numberPage.toString();
+    // this.getSummary(31,this.selectedValueSource, this.pageSummary);
+  }
+
+  latestPage() {
+    this.numberPage = this.numberPages;
+    this.pageSummary.page = this.numberPage.toString();
+    // this.getSummary(31,this.selectedValueSource, this.pageSummary);
+  }
+
+
+  regresar() {
+    
+  }
 }
