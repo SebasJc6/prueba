008e0f3f752621af540f900192cc27b78e247fb1
M-Narvaez-SOAINT <113195440+M-Narvaez-SOAINT@users.noreply.github.com>
2022-11-21 20:18:03 -0500

    Ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,4 +7,4 @@ export const environment = {
     logic:"https://wa-aeu-sds-qas-wapi-back.azurewebsites.net/api/v1/",
     generic:"https://wa-aeu-sds-qas-wapi-canonical.azurewebsites.net/api/v1/"
   } 
-  };
+};
