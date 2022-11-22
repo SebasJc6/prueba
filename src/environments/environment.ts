1680c1843890e9f50a4778230172025993b2455c
devopsETB SDS <devopsETB@saludcapital.gov.co>
2022-11-22 01:25:41 +0000

    Ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
-
 export const environment = {
-  production: false,
-
-  baseUrl:{
-    auth : "https://login-appservice-back2.azurewebsites.net/",
-    logic:"https://wa-aeu-sds-qas-wapi-back.azurewebsites.net/api/v1/",
-    generic:"https://wa-aeu-sds-qas-wapi-canonical.azurewebsites.net/api/v1/"
-  } 
-};
+    production: false,
+    
+    baseUrl:{
+      auth : "https://login-appservice-back2.azurewebsites.net/",
+      logic:"https://wa-aeu-sds-qas-wapi-back.azurewebsites.net/api/v1/",
+      generic:"https://wa-aeu-sds-qas-wapi-canonical.azurewebsites.net/api/v1/"
+    } 
+  };
+  
